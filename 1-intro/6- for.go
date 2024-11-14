50a1f511664351eaab497d4788171edf5f716ae3
Jorge Ga <asbrax.go@gmail.com>
2024-11-14 14:56:03 -0600

    push multiples y fors

---
+++ 1-intro/6- for.go
@@ -0,0 +1,38 @@
+// `for` is Go's only looping construct. Here are
+// some basic types of `for` loops.
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// The most basic type, with a single condition.
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+    fmt.Println("--------")
+	for j := 3; j > 0; j-- {
+		fmt.Println(j)
+	}
+
+	
+    
+    fmt.Println("--------")
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+    
+    fmt.Println("--------")
+	for n := range 6 {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+}
